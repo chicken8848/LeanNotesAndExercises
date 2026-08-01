11862116bf9f87b97c83ae64791c4aca917b73e7
chicken8848 <chicken8848@hotmail.com>
2026-08-01 15:11:19 +0800

    something

---
+++ MyLeanProject/MIL/2-Basics.lean
@@ -0,0 +1,9 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Analysis.SpecialFunctions.Pow.Real -- or Mathlib.Algebra.Order.Field.Basic
+
+example (a b c : ℝ) : c * b * a = b * (a * c) := by
+  sorry
+
+example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
+  sorry
+
